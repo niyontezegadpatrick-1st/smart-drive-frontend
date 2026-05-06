31c9c97df092c7ed3cd58c4eddce7f660dc68331
niyontezegadpatrick-1st <niyontezegadpatrick@gmail.com>
2026-05-06 15:18:55 +0200

    Fix AuthContext path in ProtectedRoute - capital C

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from "../../Context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { currentUser, loading } = useContext(AuthContext);
